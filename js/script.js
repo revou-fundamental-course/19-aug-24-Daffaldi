f51d530c1b09fab2004c3489a8060e6736c4e120
Daffa Naufal Zuhdi <a52backup.2022@gmail.com>
2024-08-27 09:12:10 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Check if we are on the home page
     if (window.location.pathname === '/' || window.location.pathname.endsWith('index.html')) {
-        // Welcome message personalization
         const welcomeMessage = document.getElementById('welcomeMessage');
         if (welcomeMessage) {
             const userName = localStorage.getItem('userName');
-
-            // If the userName is not set, prompt the user to enter their name
             if (!userName) {
                 const enteredName = prompt('Please enter your name:');
                 if (enteredName) {
-                    // Store the entered name in localStorage
                     localStorage.setItem('userName', enteredName);
                     document.getElementById('welcomeMessage').textContent = `Hi ${enteredName}, Welcome To Website`;
                 }
             } else {
-                // If the userName is already stored, display it directly
                 document.getElementById('welcomeMessage').textContent = `Hi ${userName}, Welcome To Website`;
             }
         }
     }
 
-    // Form validation and submission (applies to all pages)
     const contactForm = document.getElementById('messageForm');
-    if (contactForm) {  // Check if the form exists on the page
+    if (contactForm) {
         contactForm.addEventListener('submit', function(event) {
             event.preventDefault();
             const name = document.getElementById('name').value;
